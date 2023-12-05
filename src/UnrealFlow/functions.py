62edfaf69aa95eb94d2f6990981850621f22d318
Advik <advik.b@gmail.com>
2023-12-05 20:19:49 +0530

    Create functions.py

---
+++ src/UnrealFlow/functions.py
@@ -0,0 +1,59 @@
+"""
+# MAIN FUNCTIONS
+print
+input
+log
+exit
+raise
+try
+catch
+dir
+
+# Loops
+for
+while
+foreach
+break
+continue
+
+# Conditionals
+if
+elif
+else
+switch
+case
+default
+
+# Operators
+and
+or
+not
+is
+in
+
+# Data Types
+int
+float
+str
+bool
+array
+json
+
+# Special
+import
+const
+config
+
+# math
+abs
+min
+max
+round
+ceil
+floor
+clamp
+lerp
+random
+randint
+randfloat
+"""
