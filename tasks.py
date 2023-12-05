086eb1295a5a59a4e114ae29cff1887e043e2998
Advik <advik.b@gmail.com>
2023-12-05 18:03:11 +0530

    Create tasks.py

---
+++ tasks.py
@@ -0,0 +1,23 @@
+from invoke import task
+import os
+
+def is_windows():
+    return os.name == 'nt'
+
+
+@task
+def check_compiler(c):
+    PATH = os.environ['PATH']
+    PROTOC = 'protoc'
+    if is_windows():
+        PROTOC += '.exe'
+    for path in PATH.split(os.pathsep):
+        if os.path.exists(os.path.join(path, PROTOC)):
+            return
+    raise RuntimeError("protoc not found")
+
+
+@task
+def proto(c, docs=False):
+    check_compiler(c)
+    c.run("protoc proto/*.proto --python_out=src/")
