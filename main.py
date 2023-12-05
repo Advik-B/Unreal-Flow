84ed11eab991a9b9f1037cffd35e23b7568c119b
Advik <advik.b@gmail.com>
2023-12-05 17:40:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,3 @@
-from protobuf.nodes_pb2 import (
-    BaseNode,
-    NodeGraph,
-    NodeType,
-    PrintNode,
-)
-
-node_graph = NodeGraph()
-
-for i in range(10):
-    # Add PrintNode
-    print_node = PrintNode()
-    print_node.base.name = f"PrintNode_{i}"
-    print_node.base.type = NodeType.PRINT
-    print_node.message = f"Hello World {i}"
-    node_graph.nodes.append(print_node)
-
-
-
-print(node_graph)
-
-# Export to file
-with open("node_graph.bin", "wb") as f:
-    f.write(node_graph.SerializeToString())
+from google.protobuf import text_format
+from protobuf import nodes_pb2
 
